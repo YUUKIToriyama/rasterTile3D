47a4b21102e7492d684b69ae7d5c0fd36f8f0b71
YUUKIToriyama <github@toriyama.dev>
2021-10-25 02:19:04 +0900

    ラスタータイルを読み込み簡易的な色付けができるようにした

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import * as Babylon from 'babylonjs';
 import parse from 'csv-parse/lib/sync';
+import getPixels from 'get-pixels';
 
 const dom = {
 	canvas: document.getElementById("render3d") as HTMLCanvasElement,
 	inputZoomLevel: document.getElementById("zoomLevel") as HTMLInputElement,
 	inputCoordX: document.getElementById("coord_x") as HTMLInputElement,
-	inputCoordY: document.getElementById("coord_y") as HTMLInputElement
+	inputCoordY: document.getElementById("coord_y") as HTMLInputElement,
 }
 
 const getDemTile = async (zoomLevel: number, x: number, y: number) => {
@@ -19,6 +20,20 @@ const getDemTile = async (zoomLevel: number, x: number, y: number) => {
 		delimiter: ','
 	});
 	return csv;
+}
+
+const getRasterTile = async (zoomLevel: number, x: number, y: number): Promise<Uint8Array> => {
+	// 地理院全国ランドサットモザイク画像にアクセス
+	const url = `https://cyberjapandata.gsi.go.jp/xyz/lndst/${zoomLevel}/${x}/${y}.png`;
+	return new Promise((resolve, reject) => {
+		getPixels(url, (error, pixels) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(pixels.data as Uint8Array);
+			}
+		})
+	});
 }
 
 // キャンバスを取得
@@ -49,8 +64,8 @@ const initializeScene = () => {
 }
 
 // 標高タイルのデータからポリゴンを作成
-const createPolygons = (scene: Babylon.Scene, demTile: number[][]) => {
-	const dest = 3;
+const createPolygons = (scene: Babylon.Scene, demTile: number[][], pixelData?: Uint8Array) => {
+	const dest = 5;
 	for (let n = 0; n < 256; n = n + dest) {
 		for (let m = 0; m < 256; m = m + dest) {
 			const height = demTile[n][m] / 10;
@@ -63,6 +78,17 @@ const createPolygons = (scene: Babylon.Scene, demTile: number[][]) => {
 			column.position.x = n - 128;
 			column.position.z = m - 128;
 			column.position.y = height / 2;
+			// ラスター画像がある場合はポリゴンに色付けを行なう
+			if (pixelData !== undefined) {
+				const index = (n + m * 256) * 4;
+				const material = new Babylon.StandardMaterial(`material-${n}-${m}`, scene);
+				material.diffuseColor = new Babylon.Color3(
+					pixelData[index] / 256,
+					pixelData[index + 1] / 256,
+					pixelData[index + 2] / 256
+				);
+				column.material = material;
+			}
 		}
 	}
 
@@ -77,12 +103,14 @@ window.addEventListener("resize", () => {
 });
 
 
-document.getElementById("showButton").addEventListener("click", () => {
+document.getElementById("showButton").addEventListener("click", async () => {
 	const zoomLevel = parseInt(dom.inputZoomLevel.value);
 	const coord_x = parseInt(dom.inputCoordX.value);
 	const coord_y = parseInt(dom.inputCoordY.value);
-	getDemTile(zoomLevel, coord_x, coord_y).then(demTile => {
-		scene = initializeScene();
-		createPolygons(scene, demTile);
-	})
+
+	const demTile = await getDemTile(zoomLevel, coord_x, coord_y);
+	const pixelData = await getRasterTile(zoomLevel, coord_x, coord_y);
+	console.log(pixelData);
+	scene = initializeScene();
+	createPolygons(scene, demTile, pixelData);
 });
